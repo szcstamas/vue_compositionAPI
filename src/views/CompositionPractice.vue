43396ab03fd9bfc3f936cc9f9863de09794e442a
Tamás Szűcs <x-tamas.szucs2@wizzair.com>
2023-04-26 15:07:27 +0200

    Adding computed property to CompositionPractice view

--- src/views/CompositionPractice.vue
+++ src/views/CompositionPractice.vue
@@ -5,22 +5,35 @@
             :sectionH4="'2023.04.26'"
             :sectionSubline="'Just another routed page!'"
         >
-            <p class="primary-text">Capacity: <strong>{{ capacity }}</strong></p>
+            <p class="primary-text">Spaces Left: {{ spacesLeft }} out of <strong>{{ capacity }}</strong></p>
             <button @click="increaseCapacity()">Increase Capacity</button>
+            <div class="attending-div">
+                <h2>Attending</h2>
+                <ul>
+                    <li v-for="(name, index) in attending" :key="index">
+                        {{ name }}
+                    </li>
+                </ul>
+            </div>
         </CompositionSection>
     </main>
 </template>
 
 <script>
 import CompositionSection from "@/components/CompositionSection.vue"
-import { ref } from "vue"
+import { ref, computed } from "vue"
 
 export default {
     setup() {
         const capacity = ref(3);
+        const attending = ref(["Tim", "Bob", "Joe"]);
 
-        function increaseCapacity() { capacity.value++ }
-        return { capacity, increaseCapacity }
+        const spacesLeft = computed(() => {
+            return capacity.value - attending.value.length
+        })
+
+        function increaseCapacity() { capacity.value++; }
+        return { capacity, increaseCapacity, attending, spacesLeft };
     },
     components: {
         CompositionSection
@@ -28,6 +41,14 @@ export default {
 }
 </script>
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
+.attending-div {
+    margin-top: 2rem;
+
+    ul {
+        margin-top: 1rem;
+        padding-left: 2rem;
+    }
+}
 
 </style>
