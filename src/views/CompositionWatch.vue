8012eca903a35210618ed28a7b8b14ecb651e319
Tamás Szűcs <x-tamas.szucs2@wizzair.com>
2023-04-27 16:07:47 +0200

    Adding vuex to project

--- src/views/CompositionWatch.vue
+++ src/views/CompositionWatch.vue
@@ -5,21 +5,25 @@
             :sectionH4="'2023.04.27.'"
             :sectionSubline="'Practicing Composition API with watchEffect'"
         >
-        <div>
-            Search for <input type="text" placeholder="something..." v-model="searchInput">
-            <p style="margin-top:1rem;">Searched keyword: {{ results }}</p>
-        </div>
+            <div>
+                Search for <input type="text" placeholder="something..." v-model="searchInput">
+                <p style="margin-top:1rem;">Searched keyword: {{ results }}</p>
+            </div>
         </CompositionSection>
     </main>
 </template>
 
 <script>
 import CompositionSection from "@/components/CompositionSection.vue";
-import { ref, watchEffect, watch } from "vue";
+import { ref, watch } from "vue";
+import { mapState } from "vuex";
 
 export default {
     components: {
         CompositionSection
+    },
+    computed: {
+
     },
     setup() {
         const searchInput = ref("");
@@ -28,7 +32,7 @@ export default {
         //LAZY LOAD OF WATCH
         watch(searchInput, () => {
             searchInput.value.length > 0 ? results.value = searchInput.value : results.value = "waiting for keyword...";  
-        });
+        }, { immediate: true });
 
         //IMMEDIATE LOAD OF WATCH
         // watchEffect(() => {
